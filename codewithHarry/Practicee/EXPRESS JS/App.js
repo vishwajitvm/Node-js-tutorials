39147c6d26025ec25f13d7c03656c05823839505
vishwajitvm <vishwajitmall50@gmail.com>
2023-05-05 00:28:14 +0530

    basics of pug templete day 1

--- codewithHarry/Practicee/EXPRESS JS/App.js
+++ codewithHarry/Practicee/EXPRESS JS/App.js
@@ -3,34 +3,26 @@ const path = require('path')
 const app = express() ;
 const port = 80 ;
 
+//EXPRESS SPECIFIC CONMFIG
 app.use('/static', express.static('static'))
 
-//set the templete engine for pug
+//PUB SPECIFIC CONFIG
 app.set('view engine', 'pug')
-
-//set the iew directory
 app.set('views' , path.join(__dirname , 'views')) ;
 
-//our pug end poit
-app.get('/demo', (req, res) => {
-    res.status(200).render('demo', { title: 'Hey vishwajit', message: 'Hello there!!!demo' })
-  })
 
+//ENDPOINTS
 app.get('/' , (req, res) => {
-    // res.send("First app with vishwajit vm")
-    res.status(200).send("First app with vishwajit vm")
+    const message = "Vishwajit vm Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit molestiae ut animi?" ;
+    const param = {"title" : "Vishwajit vm website" , "message" : message}
+    res.status(200).render("index.pug" , param)
 })
 
-app.get('/about' , (req, res) => {
-    res.send("About Page with get")
-})
+app.get('/demo', (req, res) => {res.status(200).render('demo', { title: 'Hey vishwajit', message: 'Hello there!!!demo' })})
+app.get('/about' , (req, res) => {res.send("About Page with get")})
+app.post('/about' , (req, res) => {res.send("About Page with post")})
 
-app.post('/about' , (req, res) => {
-    res.send("About Page with post")
-})
-
-
-
+//START THE SERVER
 app.listen(port , () => {
     console.log('====================================');
     console.log(`The application Started Successfully on ${port}`);
