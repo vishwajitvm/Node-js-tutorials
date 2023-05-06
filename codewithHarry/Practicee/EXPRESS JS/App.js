3215d0d42395f0b1bdc5bea2362630d797e12323
vishwajitvm <vishwajitmall50@gmail.com>
2023-05-06 11:58:51 +0530

    get post form data in console and genrate a file with formdata in express js

--- codewithHarry/Practicee/EXPRESS JS/App.js
+++ codewithHarry/Practicee/EXPRESS JS/App.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const fs = require('fs') ;
 const path = require('path')
 const app = express() ;
 const port = 80 ;
 
 //EXPRESS SPECIFIC CONMFIG
-app.use('/static', express.static('static'))
+app.use('/static', express.static('static')) //for serving static file
+app.use(express.urlencoded()) ;
 
 //PUB SPECIFIC CONFIG
 app.set('view engine', 'pug')
@@ -17,6 +19,19 @@ app.get('/' , (req, res) => {
     const param = {"title" : "Vishwajit vm website" , "message" : message}
     res.status(200).render("index.pug" , param)
 })
+
+app.post('/' , (req, res) => {
+    var formdata = req.body ;
+    var movietitle = formdata.movietitle ;
+    var year = formdata.year ;
+    var imdb = formdata.imdb ;
+
+    var outPutToWrite = `The name of movie is ${movietitle} and it was relesed in year ${year} , with overall raiting of ${movietitle} is around ${imdb}!!Hurry go watch`
+    fs.writeFileSync(`${movietitle}_${year}_review.txt` , outPutToWrite) ;
+    const param = {"title" : "YOur message save successfully" }
+    res.status(200).render("index.pug" , param)
+})
+
 
 app.get('/demo', (req, res) => {res.status(200).render('demo', { title: 'Hey vishwajit', message: 'Hello there!!!demo' })})
 app.get('/about' , (req, res) => {res.send("About Page with get")})
