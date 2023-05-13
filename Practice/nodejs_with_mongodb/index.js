db9170a32e86951cd261f173d065ffef78a6c7b1
vishwajitvm <vishwajitmall50@gmail.com>
2023-05-13 12:36:39 +0530

    connect mongo with node js , and also get the record from products collection in e-comm database

--- Practice/nodejs_with_mongodb/index.js
+++ Practice/nodejs_with_mongodb/index.js
@@ -1,2 +1,40 @@
 const express = require('express') ;
-const 
+const {MongoClient} = require('mongodb') ;
+const mongoose = require('mongoose') ;
+const url = 'mongodb://127.0.0.1:27017' ;
+const database = 'e-comm' ;
+const client = new MongoClient(url) ;
+
+const port = process.env.port || 8080 ;
+const app = express() ;
+let time = new Date();
+//Views
+app.set('view engine' , 'ejs')
+
+//Static file[css]
+app.use(express.static('public'))
+
+//MONGO DB CONNECTION mongodb://localhost:27017
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/e-comm' , {useNewUrlParser: true});
+    console.log('=================================================================================');
+    console.log(`Mongo DB is  Connect ${time. getHours() + ":" + time}`);
+    console.log('=================================================================================');
+}
+
+
+//get dat from mongodb
+async function getData() {
+    const client = new MongoClient(url);
+    await client.connect();
+    const db = client.db(database);
+    const collection = db.collection('products');
+    const response = await collection.find({}).toArray();
+    console.log(response);
+  }
+  
+  getData();
+
+app.listen(port) ;
